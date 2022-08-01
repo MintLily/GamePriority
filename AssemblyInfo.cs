b99ce4cbe6e7325f67114bcea5cb1f6e85d9f91e
Lily <admin@mintlily.lgbt>
2022-08-01 11:39:46 -0400

    Fresh project

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using GamePriority;
+
+[assembly: AssemblyTitle(GamePriorityChanger.BuildInfo.Name)]
+[assembly: AssemblyDescription("Using MelonLoader, this is a universal mod that allows you to set the game's process priority with either Normal or High")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(GamePriorityChanger.BuildInfo.Company)]
+[assembly: AssemblyProduct(GamePriorityChanger.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + GamePriorityChanger.BuildInfo.Author)]
+[assembly: AssemblyTrademark(GamePriorityChanger.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("76A74B7D-000B-42AC-8F48-60E5D449CA91")]
+[assembly: AssemblyVersion(GamePriorityChanger.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(GamePriorityChanger.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(GamePriorityChanger), GamePriorityChanger.BuildInfo.Name, GamePriorityChanger.BuildInfo.Version, GamePriorityChanger.BuildInfo.Author, GamePriorityChanger.BuildInfo.DownloadLink)]
+[assembly: MelonGame]
---
+++ README.md
@@ -0,0 +1,24 @@
+# GamePriority
+Using **MelonLoader**, this is a universal mod that allows you to set the game's process priority with either Normal or High
+
+### MelonLoader
+Need to install MelonLoader?<br>
+Click [this link](https://melonwiki.xyz/) to get started!
+
+## Mod Info
+**Mod Version**: v2.0.0<br>
+**MelonLoader Version**: 0.5.4+
+
+### Tested Games
+- BONEWORKS
+- VRChat (EAC Prevention)
+- Risk of Rain 2
+- NeosVR
+- Arizona Sunshine
+- VTOL VR
+- Ultimate Epic Battle Simulator
+- Totally Accurate Battle Simulator
+- SynthRiders
+- Audica
+- Beat Saber _(Yes, with ML on top of IPA)_
+- ChilloutVR
